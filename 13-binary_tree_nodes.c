135a346e1293ba1dade01ba476a6f3c5e3cbe951
Holberton <flockwood@live.com>
2025-04-10 16:39:21 -0400

    fix betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in the binary tree
+ * binary_tree_nodes - Counts the nodes with at least 1 child in the tree
  * @tree: Pointer to the root node of the tree to count the nodes.
  *
  * Return: The number of nodes with at least 1 child
